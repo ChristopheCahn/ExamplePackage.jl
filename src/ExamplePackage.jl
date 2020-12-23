1ca442f4efb1822b11b5070d7f07023fe5242527
Christophe Cahn (MacBookPro) <christophe.cahn@gmail.com>
2020-12-23 11:49:56 +0100

    premieres fonctions

--- src/ExamplePackage.jl
+++ src/ExamplePackage.jl
@@ -1,5 +1,15 @@
 module ExamplePackage
 
-# Write your package code here.
+greet() = print("Hello World!")
+
+using Expectations, Distributions
+
+function foo(μ = 1., σ = 2.)
+    d = Normal(μ, σ)
+    E = expectation(d)
+    return E(x -> sin(x))
+end
+
+export foo
 
 end
